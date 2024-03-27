a16dc74c382e7a0b28aefc0628be045d482f7669
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-27 11:07:11 +0200

    add routers for all views in theatre\views.py

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,27 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import (
+    ActorViewSet,
+    GenreViewSet,
+    PlayViewSet,
+    ReservationViewSet,
+    TheatreHallViewSet,
+    PerformanceViewSet,
+    TicketViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("plays", PlayViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
 
 app_name = "theatre"
-
-urlpatterns = []
