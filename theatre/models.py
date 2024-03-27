eb3fa9de6410b4322ba2e80a2005cce3e3c514a1
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-27 09:53:06 +0200

    add str representation to all models in theatre\models.py

--- theatre/models.py
+++ theatre/models.py
@@ -8,9 +8,15 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
 
 
 class Play(models.Model):
@@ -19,6 +25,9 @@ class Play(models.Model):
     actors = models.ManyToManyField(Actor, related_name="plays")
     genres = models.ManyToManyField(Genre, related_name="plays")
 
+    def __str__(self):
+        return self.title
+
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -26,6 +35,9 @@ class Reservation(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return f"{self.user} {self.created_at}"
 
 
 class TheatreHall(models.Model):
@@ -37,6 +49,9 @@ class TheatreHall(models.Model):
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
+    def __str__(self):
+        return f"{self.name} - {self.capacity} capacity"
+
 
 class Performance(models.Model):
     play = models.ForeignKey(Play, related_name="performances")
@@ -46,6 +61,11 @@ class Performance(models.Model):
         on_delete=models.CASCADE
     )
     show_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"{self.play} - "
+                f"{self.theatre_hall} "
+                f"({self.show_time})")
 
 
 class Ticket(models.Model):
@@ -61,3 +81,8 @@ class Ticket(models.Model):
         related_name="tickets",
         on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return (f"{self.performance} - "
+                f"{self.reservation} "
+                f"({self.row} - {self.seat})")
