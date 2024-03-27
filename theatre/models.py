fcac1e87134566b9c9177ee9f9150a175137153f
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-27 10:36:17 +0200

    add full_name property to Actor

--- theatre/models.py
+++ theatre/models.py
@@ -7,6 +7,10 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
