1aae59060e08b8014cb840a78c3182756e3d2e05
Ihor Bondarenko <igorb6766@gmail.com>
2024-03-27 11:06:32 +0200

    add viewsets for all serializers in theatre\serializers.py

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from theatre.models import (
+    Actor,
+    Genre,
+    Play,
+    Reservation,
+    TheatreHall,
+    Performance,
+    Ticket,
+)
+from theatre.serializers import (
+    ActorSerializer,
+    GenreSerializer,
+    PlaySerializer,
+    ReservationSerializer,
+    TheatreHallSerializer,
+    PerformanceSerializer,
+    TicketSerializer
+)
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class PlayViewSet(viewsets.ModelViewSet):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
